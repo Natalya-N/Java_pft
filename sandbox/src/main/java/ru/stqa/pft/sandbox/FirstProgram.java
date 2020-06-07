d46808c12f2d3a53ad343b597949dc20259779fb
Natalya Nechaeva <nechaevanatascha@yandex.ru>
2020-06-07 22:08:03 +0400

    Task 2 done

--- sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
@@ -2,7 +2,10 @@ package ru.stqa.pft.sandbox;
 
 public class FirstProgram {
 
-public static void main (String[] args){
-System.out.println("Hello, world");}
- 
+    public static void main(String[] args) {
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(4, 5);
+
+        System.out.println(p1.distance(p2));
+    }
 }
