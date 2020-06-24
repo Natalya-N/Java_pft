17dad6412fdc24554ebde7cbba2916a2226346ac
Natalya Nechaeva <nechaevanatascha@yandex.ru>
2020-06-24 18:35:07 +0400

    Constructors replaced with builders

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -13,11 +13,10 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     appManager.goTo().groupPage();
     List<GroupData> groupsBefore = appManager.group().list();
-    GroupData group = new GroupData().withName("test2");
+    GroupData group = new GroupData().withName("test1");
     appManager.group().create(group);
     List<GroupData> groupsAfter = appManager.group().list();
     Assert.assertEquals(groupsBefore.size()+1, groupsAfter.size());
-    //group.setId(groupsAfter.stream().max((Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     groupsBefore.add(group);
     Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
     groupsBefore.sort(byId);
